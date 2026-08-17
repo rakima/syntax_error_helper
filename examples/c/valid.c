da53b02784122bb0f10525755ec4639ca82ac30b
rakima <rakima78@gmail.com>
2026-08-17 20:58:43 +0900

    add: 対応言語の検証用サンプルを追加

---
+++ examples/c/valid.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int value = 10;
+
+    if (value > 5) {
+        printf("value = %d\n", value);
+    }
+
+    return 0;
+}
---
+++ examples/java/ValidSample.java
@@ -0,0 +1,9 @@
+public class ValidSample {
+    public static void main(String[] args) {
+        int value = 10;
+
+        if (value > 5) {
+            System.out.println("value = " + value);
+        }
+    }
+}
